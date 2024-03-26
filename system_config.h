64aaa0fc1fa6bf07f22c0552ed26a73025d3c994
Felix Schladt <felix.schladt@hensoldt.net>
2024-03-26 11:53:04 +0100

    [DOC] Update Copyright
    Change files to GPLv2 or Commercial licensing

--- system_config.h
+++ system_config.h
@@ -1,7 +1,11 @@
 /*
- * Copyright (C) 2020, HENSOLDT Cyber GmbH
- *
  * System libraries configurations
+ *
+ * Copyright (C) 2020-2024, HENSOLDT Cyber GmbH
+ * 
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * For commercial licensing, contact: info.cyber@hensoldt.net
  */
 
 #pragma once
